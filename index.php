8b66048558befb02d16b7541b8d39014e9d93e71
Eakapot Jewsakul <lorencias15@hotmail.com>
2017-09-29 19:46:43 +0700

    index.php

--- index.php
+++ index.php
@@ -17,12 +17,12 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			
-			$text = "สินค้าของท่านได้ถึง กทม วันที่ 29/9/2560 กรุณาติดต่อ Call Center 02-002154" . $text . "||";
+			//$text = "สินค้าของท่านได้ถึง กทม วันที่ 29/9/2560 กรุณาติดต่อ Call Center 02-002154" . $text . "||";
 			
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' =>  "สินค้าของท่านได้ถึง กทม วันที่ 29/9/2560 กรุณาติดต่อ Call Center 02-002154" . $text . "||"
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
